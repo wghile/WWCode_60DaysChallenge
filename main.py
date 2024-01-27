384085986bb5356d791cbebab48e76b90562e17a
Wentana Ghile <137902774+wghile@users.noreply.github.com>
2024-01-27 13:58:18 -0500

    changed day14 solution to account for 0,1,2 arguments

--- main.py
+++ main.py
@@ -135,15 +135,22 @@ import random
 # print(shuffled_list)
 
 # Day 14 - Write a program to print the first n numbers of a Fibonacci sequence.
-# def fibonacci(n):
-#     sequence = [0, 1]
-#     i = 0
-#     while len(sequence) < n:
-#         sum = sequence[i] + sequence[i+1]
-#         sequence.append(sum)
-#         i += 1
-#     return sequence
-# print(fibonacci(10))
+def fibonacci(n):
+    sequence = [0, 1]
+    if n == 0:
+        return []
+    elif n == 1:
+        return sequence[:1]
+    elif n == 2:
+        return sequence[:2]
+    else:
+        i = 0
+        while len(sequence) < n:
+            sum = sequence[i] + sequence[i+1]
+            sequence.append(sum)
+            i += 1
+        return sequence
+print(fibonacci(10))
 
 # Day 15 - Create a program that checks if a year is a leap year.
 # def leap_year_check(year):
@@ -164,10 +171,10 @@ import random
 # print(words_dict)
 
 # Day 17 - Write a program that capitalizes the first letter of each word in a sentence.
-sentence = input('Enter a sentence: ')
-sentence_list = sentence.split(' ')
-new_sentence = ''
-for i in range(len(sentence_list)):
-    capitalized_word = sentence_list[i].replace(sentence_list[i], sentence_list[i].title())
-    new_sentence += capitalized_word + ' '
-print(new_sentence)
+# sentence = input('Enter a sentence: ')
+# sentence_list = sentence.split(' ')
+# new_sentence = ''
+# for i in range(len(sentence_list)):
+#     capitalized_word = sentence_list[i].replace(sentence_list[i], sentence_list[i].title())
+#     new_sentence += capitalized_word + ' '
+# print(new_sentence)
